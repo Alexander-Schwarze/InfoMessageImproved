1e627ea39690e90c13723bbaeaac6bcd89f2f24a
Alexander Schwarze <shikami97@gmx.de>
2021-10-26 22:44:05 +0200

    Created Saturday/Sunday-Buttons

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,23 +1,31 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+data class Game(var gameClass: String, var gameTime: String, var missingSR: String, var missingKR: String)
+val SATURDAY = 0
+val SUNDAY = 1
+val allGames = arrayOf(emptyList<Game>(), emptyList<Game>())
 
 @Composable
 @Preview
 fun App(){
     val headlineText = remember { mutableStateOf("Start adding or generate the text")}
+    val saturdayOrSunday = remember { mutableStateOf(SATURDAY) }
 
     MaterialTheme {
         Column {
@@ -42,9 +50,45 @@ fun App(){
                     .height(150.dp)
             )
 
-            Text(
-                text = "Selection saturday / sunday"
-            )
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 20.dp, start = 20.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ){
+                val labelColors = arrayOf(Color(0xFF426eb9), Color(0xFF36393f))
+                Button(
+                    onClick = {
+                        saturdayOrSunday.value = SATURDAY
+                    },
+                    modifier = Modifier
+                        .height(40.dp)
+                        .fillMaxWidth(fraction = 0.5f),
+                    shape = RectangleShape,
+                    colors = ButtonDefaults.buttonColors(backgroundColor = labelColors[saturdayOrSunday.value])
+                ){
+                    Text(
+                        text = "Saturday",
+                        color = Color.White
+                    )
+                }
+                Button(
+                    onClick = {
+                        saturdayOrSunday.value = SUNDAY
+                    },
+                    modifier = Modifier
+                        .height(40.dp)
+                        .fillMaxWidth(fraction = 1f),
+                    shape = RectangleShape,
+                    colors = ButtonDefaults.buttonColors(backgroundColor = labelColors[(saturdayOrSunday.value + 1) % 2])
+                ){
+                    Text(
+                        text = "Sunday",
+                        color = Color.White
+                    )
+                }
+            }
 
             Text(
                 text = "Input field"
